0bfc883b725f1f6d43e8aded8f2c9584afa8d705
RubinaKhanal7 <rubikhanal8@gmail.com>
2024-06-12 12:33:46 +0545

    updates

--- resources/views/backend/shared/staffs/resignationdetails.blade.php
+++ resources/views/backend/shared/staffs/resignationdetails.blade.php
@@ -17,29 +17,26 @@
                     <div class="col-sm-12 col-md-12 col-12">
                         <div class="box box-info" style="padding:5px;">
                             <div class="box-header with-border">
-
+                            </div>
+                            <div class="box-body">
+                                <div class="form-group col-lg-4 col-sm-4">
+                                    <label for="resignation_letter">Resignation Letter</label>
+                                    <input type="text" name="resignation_letter" value="{{ old('resignation_letter') }}" class="form-control" id="resignation_letter" placeholder="Enter resignation_letter" required>
+                                    @error('resignation_letter')
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-lg-3 col-sm-3 mt-2">
+                                    <label for="note">Note :</label>
+                                    <textarea name="note" class="form-control" id="note" placeholder="Note.." rows="15" cols="50">{{ old('note') }}</textarea>
+                                    @error('note')
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    @enderror
+                                </div>
                                 <div class="pull-right box-tools">
                                     <a class="btn btn-primary btn-sm">
-
-                                        <div class="form-group col-lg-4 col-sm-4">
-                                            <label for="resignation_letter">Resignation Letter</label>
-                                            <input type="text" name="resignation_letter"
-                                                value="{{ old('resignation_letter') }}" class="form-control"
-                                                id="resignation_letter" placeholder="Enter resignation_letter" required>
-                                            @error('resignation_letter')
-                                                <strong class="text-danger">{{ $message }}</strong>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group col-lg-3 col-sm-3 mt-2">
-                                            <label for="note">Note :</label>
-                                            <textarea name="note" class="form-control" id="note" placeholder="Note.." rows="15" cols="50">
-                                                        {{ old('note') }}
-                                                    </textarea>
-
-                                            @error('note')
-                                                <strong class="text-danger">{{ $message }}</strong>
-                                            @enderror
-                                        </div>
+                                        Submit <!-- Add text for the button -->
+                                    </a> <!-- Closing tag for the button -->
                                 </div>
                             </div>
                         </div>
@@ -48,4 +45,4 @@
             </div>
         </div>
     </div>
-    @endsection
+@endsection
