8c5fbd7e69870d54b1b441c9e46f87c8d3f9cb0d
sadikshya1725 <sadiksha@aashatech.com>
2024-06-27 17:31:11 +0545

    municipality attendence report

--- routes/shared/attendencereport.php
+++ routes/shared/attendencereport.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\municipalityAdmin\AttendenceReportController;
 
 Route::get('/attendance-reports', [AttendenceReportController::class, 'index'])->name('attendance_reports.index');
 Route::get('/attendance-reports/report', [AttendenceReportController::class, 'report'])->name('attendance_reports.report');
+Route::get('admin/attendance-reports/data', [AttendenceReportController::class, 'getData'])->name('attendance_reports.data');
+
+
